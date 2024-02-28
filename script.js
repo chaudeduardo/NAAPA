3c9e4ea661725c50ee5550fbd6caba5cebfafa23
chaudeduardo <161602366+chaudeduardo@users.noreply.github.com>
2024-02-28 13:16:54 -0300

    Update script.js

--- script.js
+++ script.js
@@ -14,7 +14,6 @@ var dreMappings = {
     "SA": "Santo Amaro",
     "SM": "São Mateus",
 };
-
 // Função para preencher a lista suspensa das UEs com base na DRE selecionada
 function preencherListaUEs(dreSelecionada) {
     // Seleciona o elemento do HTML para a lista suspensa das UEs
@@ -46,58 +45,3 @@ document.getElementById('menu').addEventListener('change', function() {
     // Chama a função para preencher a lista suspensa das UEs com base na DRE selecionada
     preencherListaUEs(dreSelecionada);
 });
-
-// Função para obter a localização do usuário
-function obterLocalizacao() {
-    if (navigator.geolocation) {
-        function success(position) {
-            var latitude = position.coords.latitude;
-            var longitude = position.coords.longitude;
-            // Preenche o campo de localização com a latitude e longitude
-            document.getElementById("ilocal").value = "Latitude: " + latitude + ", Longitude: " + longitude;
-        }
-        function error(err) {
-            console.log(err);
-        }
-        // Solicite a localização apenas quando o botão for clicado
-        navigator.geolocation.getCurrentPosition(success, error, {
-            enableHighAccuracy: true,
-            timeout: 5000
-        });
-    } else {
-        alert("Geolocalização não é suportada neste navegador.");
-    }
-}
-
-// Função para validar o formulário
-function validarFormulario() {
-    // Obter referências às caixas de seleção
-    var checkbox1 = document.getElementById("ientrada");
-    var checkbox2 = document.getElementById("isaida");
-    
-    // Verificar se pelo menos uma caixa de seleção está marcada
-    if (!checkbox1.checked && !checkbox2.checked) {
-        alert("Selecione pelo menos uma opção.");
-        return false; // Impedir o envio do formulário
-    }
-    return true; // Permitir o envio do formulário
-}
-
-// Captura o campo de localização e o botão de envio
-const localizacaoInput = document.getElementById('ilocal');
-const botaoEnviar = document.getElementById('isubimit');
-
-// Função para verificar se o campo de localização está vazio ou preenchido
-function verificarCampoLocalizacao() {
-    if (localizacaoInput.value === '') {
-        botaoEnviar.setAttribute('disabled', 'true');
-    } else {
-        botaoEnviar.removeAttribute('disabled');
-    }
-}
-
-// Adiciona um ouvinte de evento ao campo de localização
-localizacaoInput.addEventListener('input', verificarCampoLocalizacao);
-
-// Verifica o estado do campo de localização a cada 500ms
-setInterval(verificarCampoLocalizacao, 500);
