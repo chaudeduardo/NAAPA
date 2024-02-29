26679244e35df1be45b0449cb8cb23ad842cae37
chaudeduardo <161602366+chaudeduardo@users.noreply.github.com>
2024-02-29 10:19:28 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,16 @@
-// Mapeamento entre DREs abreviadas e seus nomes completos
-var dreMappings = {
-    "BT": "Butantã",
-    "CL": "Campo Limpo",
-    "CS": "Capela do Socorro",
-    "FB": "Freguesia/Brasilândia",
-    "G": "Guaianases",
-    "IP": "Ipiranga",
-    "IQ": "Itaquera",
-    "JT": "Jaçanã/Tremembé",
-    "MP": "São Miguel Paulista",
-    "PE": "Penha",
-    "PJ": "Pirituba/Jaçanã",
-    "SA": "Santo Amaro",
-    "SM": "São Mateus",
-};
 // Função para preencher a lista suspensa das UEs com base na DRE selecionada
 function preencherListaUEs(dreSelecionada) {
     var selectUE = document.getElementById('unidadeEducacionalDropdown');
     selectUE.innerHTML = '<option value="" selected>Selecione a Unidade Educacional</option>';
-    var unidadesEducacionais = dreMappings[dreSelecionada];
-    if (unidadesEducacionais) {
-        unidadesEducacionais.forEach(function(ue) {
-            selectUE.innerHTML += '<option value="' + ue + '">' + ue + '</option>';
-        });
-        selectUE.removeAttribute('disabled');
+    var dreAbreviada = dreSelecionada;
+    if (dreAbreviada) {
+        // Aqui você pode usar a abreviatura da DRE para buscar as UEs na sua planilha
+        // Exemplo:
+        // dreAbreviada = "BT"; // Supondo que a DRE selecionada seja Butantã
+        // Agora você pode usar "BT" para buscar as UEs correspondentes na planilha
+        // Após obter as UEs, você pode preencher a lista suspensa como estava fazendo antes
     } else {
-        console.log('Nenhuma UE encontrada para a DRE selecionada: ' + dreSelecionada);
+        console.log('Abreviatura da DRE não encontrada para: ' + dreSelecionada);
     }
 }
 
@@ -49,7 +34,5 @@ document.getElementById('menu').addEventListener('change', function() {
     var dreSelecionada = this.value;
     
     // Chama a função para preencher a lista suspensa das UEs com base na DRE selecionada
-    preencherListaUEs(dreSelecionada);
-});Es com base na DRE selecionada
     preencherListaUEs(dreSelecionada);
 });
